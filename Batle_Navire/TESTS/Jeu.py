05b28ffd821751ec146ea24d78e76038ca9856ab
Nathan-Darene <Nathandarene85@gmail.com>
2024-06-04 00:18:09 +0000

    Jeu pour l'invite de commande

---
+++ Batle_Navire/TESTS/Jeu.py
@@ -0,0 +1,54 @@
+import random
+
+from Grille import *
+from Bateau import *
+# from Joueur import *
+# from IA import *
+class Jeu:
+    def __init__(self):
+        self.grille_joueur = Grille()
+        self.grille_ordinateur = Grille()
+        self.bateaux = [
+            ("Porte-avions", 5),
+            ("Croiseur", 4),
+            ("Contre-torpilleur", 3),
+            ("Sous-marin", 3),
+            ("Torpilleur", 2)
+        ]
+
+    def placer_bateaux_aleatoire(self, grille):
+        for nom, taille in self.bateaux:
+            place = False
+            while not place:
+                orientation = random.choice(["horizontal", "vertical"])
+                if orientation == "horizontal":
+                    x = random.randint(0, 9)
+                    y = random.randint(0, 9 - taille)
+                    positions = [(x, y + i) for i in range(taille)]
+                else:
+                    x = random.randint(0, 9 - taille)
+                    y = random.randint(0, 9)
+                    positions = [(x + i, y) for i in range(taille)]
+                if all(grille.grille[x][y] == 0 for (x, y) in positions):
+                    grille.ajouter_bateau(Bateau(nom, taille), positions)
+                    place = True
+
+    def jouer(self):
+        self.placer_bateaux_aleatoire(self.grille_joueur)
+        self.placer_bateaux_aleatoire(self.grille_ordinateur)
+        tour_joueur = True
+        while True:
+            if tour_joueur:
+                print("Votre grille :")
+                self.grille_joueur.afficher()
+                coup = input("Entrez la position (ex: B3) pour tirer: ").upper()
+                x = int(coup[1]) - 1
+                y = ord(coup[0]) - 65
+                resultat = self.grille_ordinateur.recevoir_tir(x, y)
+                print(resultat)
+            else:
+                x = random.randint(0, 9)
+                y = random.randint(0, 9)
+                resultat = self.grille_joueur.recevoir_tir(x, y)
+                print(f"L'ordinateur tire en {chr(65+y)}{x+1} : {resultat}")
+            tour_joueur = not tour_joueur
